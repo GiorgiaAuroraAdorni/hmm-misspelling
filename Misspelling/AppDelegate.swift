ba764b68cda5699b275883253a58688784e032a4
Elia Cereda <eliacereda@gmail.com>
2019-06-15 21:36:03 +0200

    Created Xcode project for the GUI

---
+++ Misspelling/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Misspelling
+//
+//  Created by Elia Cereda on 15/06/2019.
+//  Copyright © 2019 Giorgia Adorni, Elia Cereda e Nassim Habbash. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Misspelling/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Misspelling
+//
+//  Created by Elia Cereda on 15/06/2019.
+//  Copyright © 2019 Giorgia Adorni, Elia Cereda e Nassim Habbash. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
