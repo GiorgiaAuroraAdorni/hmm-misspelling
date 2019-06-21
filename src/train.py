4791531ed3a04f4494421583b0e71512bfc01848
Elia Cereda <eliacereda@gmail.com>
2019-06-21 19:34:11 +0200

    Removed hmm.pickle from the repo, have Xcode generate it on demand

--- src/train.py
+++ src/train.py
@@ -1,10 +1,16 @@
+import os
+import sys
 from hmm import HMM
 
+data_dir = sys.argv[1]
+output_file = sys.argv[2]
+
 hmm = HMM(1, max_edits=2, max_states=3)
-hmm.train(words_ds="../data/word_freq/frequency-alpha-gcide.txt",
-          sentences_ds="../data/texts/big.txt",
-          typo_ds="../data/typo/new/train.csv")
+hmm.train(words_ds=os.path.join(data_dir, "word_freq", "frequency-alpha-gcide.txt"),
+          sentences_ds=os.path.join(data_dir, "texts", "big.txt"),
+          typo_ds=os.path.join(data_dir, "typo", "new", "train.csv"))
 
-hmm.save("../data/hmm.pickle")
+hmm.save(output_file)
 
-HMM.load("../data/hmm.pickle")
+# Check that the model can be loaded
+HMM.load(output_file)
