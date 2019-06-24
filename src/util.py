268f8cd2ad0e2a9f1e403dd49b3bb0ecefab54c9
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-06-24 20:08:16 +0200

    Refactored preprocessing.py and util.py.
    Added language model creation and splitted the created dataset.
    Updated datasets.

--- src/util.py
+++ src/util.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from hmm import HMM
 import numpy as np
 import random
@@ -49,19 +49,9 @@ def split_dataset(combined_csv):
     return train, test
 
 
-def perturb(perturbed, rumor_percentage):
-    # Create a model for the test set
-    hmm = HMM(1, max_edits=2, max_states=3)
-    hmm.train(words_ds="../data/word_freq/frequency-alpha-gcide.txt",
-              sentences_ds="../data/texts/lotr_clean.txt",
-              typo_ds="../data/typo/clean/test.csv")
-
-    with open("../data/texts/lotr_clean.txt", "r") as myfile:
-        cleaned = myfile.readlines()
-
+def extract_model_edit_probabilities(hmm):
     # probability that a word has an edit
     p = hmm.error_model["p"]
-    p = p * rumor_percentage # introduce a certain percentage of error (10-15-20%)
 
     # probability of the various edit
     prob_swap = hmm.error_model["swap"]
@@ -77,13 +67,67 @@ def perturb(perturbed, rumor_percentage):
 
         edit_prob[i] = edit_prob[i] + edit_prob[i - 1]
 
+    return p, edit_prob
+
+
+def perturb_word(edit_prob, indices, word, x, hmm):
+
     def substitute(word):
         l = list(word)
         if not l[indices[j]] in hmm.error_model["sub"]:
-            l[indices[j]] = random.choice(string.ascii_letters).lower()
+            l[indices[j]] = random.choice(string.ascii_letters).lower().replace(l[indices[j]], "")
         else:
-            l[indices[j]] = np.random.choice(list(hmm.error_model["sub"][l[indices[j]]].keys()))
+            l[indices[j]] = np.random.choice(list(hmm.error_model["sub"][l[indices[j]]].keys()-l[indices[j]]))
         return "".join(l)
+
+    for j in range(x):
+        r = np.random.random()
+
+        for k, e in enumerate(edit_prob):
+            if r <= edit_prob[k]:
+                break
+        value = k
+
+        # swap if you have to do only one edit
+        if value == 0 and x == 1:
+            # if the letter to switch is the last one, switch with the previous one
+            if len(indices) <= j + 1:
+                word = word[0:indices[j] - 1] + word[indices[j]] + word[indices[j] - 1] + word[indices[j] + 1:]
+            else:
+                word = word[0:indices[j]] + word[indices[j] + 1] + word[indices[j]] + word[indices[j] + 2:]
+
+        # insert a letter in a random position (after idx)
+        elif value == 1:
+            new_letter = random.choice(string.ascii_letters)
+            word = word[0:indices[j]] + new_letter + word[indices[j] + 1:]
+
+        # delete a letter
+        elif value == 2:
+            if len(word) == 1:
+                # if the word is 1 char, don't delete the word but substitute it with another one
+                word = substitute(word)
+            else:
+                word = word[0:indices[j]] + word[indices[j] + 1:]
+
+        # substitute a letter
+        else:
+            word = substitute(word)
+    return word
+
+
+def perturb_file(perturbed, rumor_percentage):
+    # Create a model for the test set
+    hmm = HMM(1, max_edits=2, max_states=3)
+    hmm.train(words_ds="../data/word_freq/lotr_language_model.txt",
+              sentences_ds="../data/texts/lotr_clean.txt",
+              typo_ds="../data/typo/clean/test.csv")
+
+    with open("../data/texts/lotr_clean.txt", "r") as myfile:
+        cleaned = myfile.readlines()
+
+    p, edit_prob = extract_model_edit_probabilities(hmm)
+
+    p = p * rumor_percentage  # introduce a certain percentage of error (10-15-20%)
 
     print("\n Starting pertub…")
     start = time.time()
@@ -101,38 +145,7 @@ def perturb(perturbed, rumor_percentage):
             indices = np.random.choice(n, x, replace=False)
             indices = -np.sort(-indices)
 
-            for j in range(x):
-                r = np.random.random()
-
-                for k, e in enumerate(edit_prob):
-                    if r <= edit_prob[k]:
-                        break
-                value = k
-
-                # swap if you have to do only one edit
-                if value == 0 and x == 1:
-                    # if the letter to switch is the last one, switch with the previous one
-                    if len(indices) <= j + 1:
-                        word = word[0:indices[j] - 1] + word[indices[j]] + word[indices[j] - 1] + word[indices[j] + 1:]
-                    else:
-                        word = word[0:indices[j]] + word[indices[j] + 1] + word[indices[j]] + word[indices[j] + 2:]
-
-                # insert a letter in a random position (after idx)
-                elif value == 1:
-                    new_letter = random.choice(string.ascii_letters)
-                    word = word[0:indices[j]] + new_letter + word[indices[j] + 1:]
-
-                # delete a letter
-                elif value == 2:
-                    if len(word) == 1:
-                        # if the word is 1 char, don't delete the word but substitute it with another one
-                        word = substitute(word)
-                    else:
-                        word = word[0:indices[j]] + word[indices[j] + 1:]
-
-                # substitute a letter
-                else:
-                    word = substitute(word)
+            word = perturb_word(edit_prob, indices, word, x, hmm)
 
             line_words[i] = word
 
@@ -180,3 +193,63 @@ def split_list(lst, n):
         lst = lst[n:]
 
     return result
+
+
+def create_typo_dataset(typo_writer):
+    # Create a model for the test set
+    hmm = HMM(1, max_edits=2, max_states=3)
+    hmm.train(words_ds="../data/word_freq/lotr_language_model.txt",
+              sentences_ds="../data/texts/lotr_clean.txt",
+              typo_ds="../data/typo/clean/train.csv")
+
+    def substitute(word):
+        l = list(word)
+        if not l[indices[j]] in hmm.error_model["sub"]:
+            l[indices[j]] = random.choice(string.ascii_letters).lower().replace(l[indices[j]], "")
+        else:
+            l[indices[j]] = np.random.choice(list(hmm.error_model["sub"][l[indices[j]]].keys()-l[indices[j]]))
+        return "".join(l)
+
+    with open("../data/word_freq/lotr_language_model.txt", "r") as myfile:
+        reader = csv.reader(myfile)
+        words_ds = [row for row in reader]
+
+    typo_ds = defaultdict()
+
+    _, edit_prob = extract_model_edit_probabilities(hmm)
+
+    print("\n Starting pertub…")
+    start = time.time()
+
+    for word in words_ds:
+        word = word[0]
+        orig_word = word
+        typos = list()
+
+        for idx in range(5):
+            word = orig_word
+            n = len(word)
+            # number of errors to introduce in the word
+            if n < 3:
+                x = 1
+            else:
+                x = np.random.randint(2) + 1
+
+            # choose two letter to change
+            indices = np.random.choice(n, x, replace=False)
+            indices = -np.sort(-indices)
+
+            word = perturb_word(edit_prob, indices, word, x, hmm)
+
+            typos.append(word)
+
+        typo_ds[orig_word] = typos
+
+    end = time.time()
+    perturb_time = end - start
+
+    for key, value in typo_ds.items():
+        for v in value:
+            typo_writer.writerow([v, key])
+
+    print("Endend pertubation in {:6.2f} seconds \n".format(perturb_time))
