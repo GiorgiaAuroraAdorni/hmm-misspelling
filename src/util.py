0eebe37e69da81de07a41fd33ac741d61ac488b2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-06-26 21:30:31 +0200

    Updated preturbation function

--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 from collections import Counter, defaultdict
 from hmm import HMM
+import pandas as pd
 import numpy as np
 import random
 import string
@@ -57,39 +58,28 @@ def extract_model_edit_probabilities(hmm):
     prob_swap = hmm.error_model["swap"]
     prob_ins = hmm.error_model["ins"]
     prob_del = hmm.error_model["del"]
-    prob_sub = 1 - (prob_swap + prob_ins + prob_del)
+    prob_sub = hmm.error_model["sub"]
 
     edit_prob = [prob_swap, prob_ins, prob_del, prob_sub]
-
-    for i, e in enumerate(edit_prob):
-        if i == 0:
-            continue
-
-        edit_prob[i] = edit_prob[i] + edit_prob[i - 1]
 
     return p, edit_prob
 
 
-def perturb_word(edit_prob, indices, word, x, hmm):
+def perturb_word(edit_prob, indices, word, x):
 
     def substitute(word):
         l = list(word)
-        if not l[indices[j]] in hmm.error_model["sub"]:
+        if not l[indices[j]] in edit_prob[3]:
             l[indices[j]] = random.choice(string.ascii_letters).lower().replace(l[indices[j]], "")
         else:
-            l[indices[j]] = np.random.choice(list(hmm.error_model["sub"][l[indices[j]]].keys()-l[indices[j]]))
+            l[indices[j]] = np.random.choice(list(edit_prob[3][l[indices[j]]].keys()-l[indices[j]]))
         return "".join(l)
 
     for j in range(x):
-        r = np.random.random()
-
-        for k, e in enumerate(edit_prob):
-            if r <= edit_prob[k]:
-                break
-        value = k
+        r = np.random.randint(4)
 
         # swap if you have to do only one edit
-        if value == 0 and x == 1:
+        if r == 0 and x == 1:
             # if the letter to switch is the last one, switch with the previous one
             if len(indices) <= j + 1:
                 word = word[0:indices[j] - 1] + word[indices[j]] + word[indices[j] - 1] + word[indices[j] + 1:]
@@ -97,12 +87,15 @@ def perturb_word(edit_prob, indices, word, x, hmm):
                 word = word[0:indices[j]] + word[indices[j] + 1] + word[indices[j]] + word[indices[j] + 2:]
 
         # insert a letter in a random position (after idx)
-        elif value == 1:
-            new_letter = random.choice(string.ascii_letters)
+        elif r == 1:
+            if not word[indices[j]] in edit_prob[1]:
+                new_letter = random.choice(string.ascii_letters).lower().replace(word[indices[j]], "")
+            else:
+                new_letter = np.random.choice(list(edit_prob[1][word[indices[j]]].keys()-word[indices[j]]))
             word = word[0:indices[j]] + new_letter + word[indices[j] + 1:]
 
         # delete a letter
-        elif value == 2:
+        elif r == 2:
             if len(word) == 1:
                 # if the word is 1 char, don't delete the word but substitute it with another one
                 word = substitute(word)
@@ -129,8 +122,12 @@ def perturb_file(perturbed, rumor_percentage):
 
     p = p * rumor_percentage  # introduce a certain percentage of error (10-15-20%)
 
-    print("\n Starting pertub…")
+    print("\n Starting pertub {} …".format(perturbed.name))
     start = time.time()
+
+    typo_counter = 0
+    word_counter = 0
+    perturbed_word = 0
 
     for line in cleaned:
         line = line.replace("\n", "")
@@ -145,9 +142,14 @@ def perturb_file(perturbed, rumor_percentage):
             indices = np.random.choice(n, x, replace=False)
             indices = -np.sort(-indices)
 
-            word = perturb_word(edit_prob, indices, word, x, hmm)
+            word = perturb_word(edit_prob, indices, word, x)
 
             line_words[i] = word
+
+            if x != 0:
+                perturbed_word += 1
+            typo_counter += x
+            word_counter += 1
 
         line = " ".join(line_words)
         perturbed.write(line + '\n')
@@ -158,6 +160,13 @@ def perturb_file(perturbed, rumor_percentage):
     perturbed.close()
 
     print("Endend pertubation in {:6.2f} seconds \n".format(perturb_time))
+
+    typo_per_word = typo_counter / word_counter
+    perturbed_word_percentace = perturbed_word / word_counter
+
+    m = {'file': [perturbed.name], 'typo_percentage': [typo_per_word], 'perturbed_word_percentace': [perturbed_word_percentace], 'total_typo': [typo_counter], 'total_word': [word_counter], 'perturbed_word': perturbed_word}
+    meta = pd.DataFrame(m)
+    meta.to_csv(perturbed.name.replace('.txt', '-meta.csv'), sep=',', index=False)
 
 
 def create_model_language():
@@ -195,7 +204,9 @@ def split_list(lst, n):
     return result
 
 
-def create_typo_dataset(typo_writer):
+def create_typo_dataset(typo_ds):
+    typo_writer = csv.writer(typo_ds)
+
     # Create a model for the test set
     hmm = HMM(1, max_edits=2, max_states=3)
     hmm.train(words_ds="../data/word_freq/lotr_language_model.txt",
@@ -206,19 +217,22 @@ def create_typo_dataset(typo_writer):
         reader = csv.reader(myfile)
         words_ds = [row for row in reader]
 
-    typo_ds = defaultdict()
+    typo_dict = defaultdict()
 
     _, edit_prob = extract_model_edit_probabilities(hmm)
 
-    print("\n Starting pertub…")
+    print("\n Starting pertub {} …".format(typo_ds.name))
     start = time.time()
+
+    typo_counter = 0
+    word_counter = 0
 
     for word in words_ds:
         word = word[0]
         orig_word = word
         typos = list()
 
-        for idx in range(5):
+        for _ in range(5):
             word = orig_word
             n = len(word)
             # number of errors to introduce in the word
@@ -231,17 +245,28 @@ def create_typo_dataset(typo_writer):
             indices = np.random.choice(n, x, replace=False)
             indices = -np.sort(-indices)
 
-            word = perturb_word(edit_prob, indices, word, x, hmm)
+            word = perturb_word(edit_prob, indices, word, x)
 
             typos.append(word)
 
-        typo_ds[orig_word] = typos
+            typo_counter += x
+            word_counter += 1
+
+        typo_dict[orig_word] = typos
 
     end = time.time()
     perturb_time = end - start
 
-    for key, value in typo_ds.items():
+    for key, value in typo_dict.items():
         for v in value:
             typo_writer.writerow([v, key])
 
     print("Endend pertubation in {:6.2f} seconds \n".format(perturb_time))
+
+    typo_per_word = typo_counter / word_counter
+
+    m = {'file': [typo_ds.name], 'typo_percentage': [typo_per_word], 'total_typo': [typo_counter], 'total_word': [word_counter]}
+    meta = pd.DataFrame(m)
+    meta.to_csv(typo_ds.name.replace('.csv', '-meta.csv'), sep=',', index=False)
+
+    typo_ds.close()
